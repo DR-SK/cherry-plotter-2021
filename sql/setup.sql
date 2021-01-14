502ea30d161935aab89b7777677c4ab2afb70298
dl-watson <dlarkinwatson@gmail.com>
2021-01-14 14:03:56 -0800

    created items table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS events CASCADE;
+DROP TABLE IF EXISTS items;
 
 CREATE TABLE users (
     user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -11,6 +12,14 @@ CREATE TABLE events (
     event_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     event_name TEXT NOT NULL,
     description TEXT NOT NULL
-    -- side_effects 
+    -- side_effects (STRETCH)
+    -- room_id BIGINT REFERENCES rooms(room_id)
+);
+
+CREATE TABLE items (
+    item_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    actions TEXT[]
     -- room_id BIGINT REFERENCES rooms(room_id)
 );
