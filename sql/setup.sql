e6398a69742f10b059b59a0ae18d4c841c6d4eed
dl-watson <dlarkinwatson@gmail.com>
2021-01-14 14:01:47 -0800

    created eventstable

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,16 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS events;
 
 CREATE TABLE users (
     user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username TEXT NOT NULL,
     password_hash TEXT NOT NULL
+);
+
+CREATE TABLE events (
+    event_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    event_name TEXT NOT NULL,
+    description TEXT NOT NULL
+    -- side_effects 
+    -- room_id BIGINT REFERENCES rooms(room_id)
 );
