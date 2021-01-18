c3046f82bf1c88dd2e75f093bd52e800ef62f17d
dl-watson <dlarkinwatson@gmail.com>
2021-01-18 11:26:13 -0800

    create game.test.js

---
+++ __tests__/game.test.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const pool = require("../lib/utils/pool");
+const request = require("supertest");
+const app = require("../lib/app");
+
+describe("test inventory routes", () => {
+  beforeEach(async () => {
+    await pool.query(fs.readFileSync("./sql/setup.sql", "utf-8"));
+    await pool.query(fs.readFileSync("./sql/game.test.sql", "utf-8"));
+  });
+
+  afterAll(() => {
+    return pool.query.end();
+  });
+});
