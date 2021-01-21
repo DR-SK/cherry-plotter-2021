99475c6fd3e308b1d27c353db5ffa401ea4482c7
sarah-svg <69824391+sarah-svg@users.noreply.github.com>
2021-01-21 15:01:46 -0800

    Feature/action test (#46)
    * test for action
    
    * second test actions
    
    * test3 actions

---
+++ __tests__/action.test.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+const pool = require("../lib/connection/pool");
+const request = require("supertest");
+const app = require("../lib/app");
+const UserService = require("../lib/services/UserService");
+
+describe.only("test actions test routes", () => {
+  let user;
+  const agent = request.agent(app);
+  let newGame;
+
+  beforeEach(async() => {
+    await pool.query(fs.readFileSync("./sql/setup.sql", "utf-8"));
+    await pool.query(fs.readFileSync("./sql/seed.sql", "utf-8"));
+    user = await UserService.create({ username:"username", password: "password" });
+    
+    await agent 
+      .post("/api/v1/auth/login")
+      .send({ username:"username", password: "password" });
+      
+    newGame = await agent 
+      .get("/games/new/");
+  });
+
+  afterAll(() => {
+    return pool.query.end();
+  });
+
+  it("shows a user a list of possible actions", async() => {
+    return agent
+      .post("/actions/list")
+      .send({
+        gameId: "1",
+      })
+      .then(res => {
+        expect(res.body).toEqual(["inventory", "attack", "hack", "investigate", "pick up", "use"]);
+      });
+  });
+
+  it("shows a user a list of possible targets", async() => {
+
+    return agent
+      .post("/actions/targets")
+      .send({
+        action: "inventory",
+        gameId: newGame.body.game_id,
+        roomId: newGame.body.room_id,
+
+      })
+      .then(res => {
+        expect(res.body).toEqual({ "inventory": [] });
+      });
+  });
+
+  it("shows a user a list of possible actions", async() => {
+
+    return agent
+      .post("/actions/perform")
+      .send({
+        action: "investigate", 
+        target: "grenade",
+        gameId: newGame.body.game_id,
+        userId: user.userId,
+   
+
+      })
+      .then(res => {    
+        console.log(res.body, "last test");
+        expect(res.body).toEqual();
+      });
+  });
+
+});
