1c43b75f151ac0717edcab47791a1710490ce9c3
dl-watson <dlarkinwatson@gmail.com>
2021-01-21 08:59:28 -0800

    update createUserActions

--- sql/seed.sql
+++ sql/seed.sql
@@ -25,8 +25,7 @@ COMMIT;
 BEGIN;
 
 INSERT INTO npcs (name, description, dialogue, actions, base_atk, base_hack, room_id)
-VALUES ('Dr. Chem', 'I am a Chemistry doctor', 'Hello, am doctor, dont stab with high heel pls', '{attack, hack}', 5, 10, 1),
-       ('Security Sentry', 'Wears rabbit hat', '!BANG!BANG!', '{attack, hack}', 5, 5, 1);
-
+VALUES ('Dr. Chem', 'I am a Chemistry doctor', 'Hello, am doctor, dont stab with high heel pls', '{attack, hack, investigate}', 5, 10, 1),
+       ('Security Sentry', 'Wears rabbit hat', '!BANG!BANG!', '{attack, hack, investigate}', 5, 5, 1);
 COMMIT;
 
