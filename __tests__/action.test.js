323ff6190b985db8650231331ac22306dccfcdc2
dl-watson <dlarkinwatson@gmail.com>
2021-01-21 15:39:22 -0800

    the dan method

--- __tests__/action.test.js
+++ __tests__/action.test.js
@@ -9,65 +9,66 @@ describe.only("test actions test routes", () => {
   const agent = request.agent(app);
   let newGame;
 
-  beforeEach(async() => {
+  beforeEach(async () => {
     await pool.query(fs.readFileSync("./sql/setup.sql", "utf-8"));
     await pool.query(fs.readFileSync("./sql/seed.sql", "utf-8"));
-    user = await UserService.create({ username:"username", password: "password" });
-    
-    await agent 
+
+    user = await UserService.create({
+      username: "username",
+      password: "password",
+    });
+
+    await agent
       .post("/api/v1/auth/login")
-      .send({ username:"username", password: "password" });
-      
-    newGame = await agent 
-      .get("/games/new/");
+      .send({ username: "username", password: "password" });
+
+    newGame = await agent.get("/games/new");
   });
 
   afterAll(() => {
     return pool.query.end();
   });
 
-  it("shows a user a list of possible actions", async() => {
+  it("shows a user a list of possible actions", async () => {
     return agent
       .post("/actions/list")
       .send({
-        gameId: "1",
+        gameId: newGame.body.game_id,
       })
-      .then(res => {
-        expect(res.body).toEqual(["inventory", "attack", "hack", "investigate", "pick up", "use"]);
+      .then((res) => {
+        expect(res.body).toEqual([
+          "inventory",
+          "attack",
+          "hack",
+          "investigate",
+          "pick up",
+          "use",
+        ]);
       });
   });
 
-  it("shows a user a list of possible targets", async() => {
-
+  it("shows a user a list of possible targets", async () => {
     return agent
       .post("/actions/targets")
       .send({
         action: "inventory",
         gameId: newGame.body.game_id,
         roomId: newGame.body.room_id,
-
       })
-      .then(res => {
-        expect(res.body).toEqual({ "inventory": [] });
+      .then((res) => {
+        expect(res.body).toEqual({ inventory: [] });
       });
   });
 
-  it("shows a user a list of possible actions", async() => {
-
+  it("shows a user a list of possible actions", async () => {
     return agent
       .post("/actions/perform")
       .send({
-        action: "investigate", 
+        action: "pick up",
         target: "grenade",
         gameId: newGame.body.game_id,
         userId: user.userId,
-   
-
       })
-      .then(res => {    
-        console.log(res.body, "last test");
-        expect(res.body).toEqual();
-      });
+      .then((res) => expect(res.body).toEqual(expect.anything()));
   });
-
 });
